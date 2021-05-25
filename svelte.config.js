c9909a4c23d7911fe6e8769bd897aa1852586c86
Alex McCausland <alex.mccausland@gmail.com>
2021-05-25 11:10:18 -0400

    add configuration for GitHub pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,16 @@
+import adapter from "@sveltejs/adapter-static"
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+    adapter: adapter({
+			// GitHub pages can be hosted from the root or docks folder
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null
+    })
 	}
 };
 
